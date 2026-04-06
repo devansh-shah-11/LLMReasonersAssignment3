e6d723cf7e5852b5633e8a21e2664e67e12f4bc4
Devansh Shah <devanshnshah11@gmail.com>
2026-04-06 12:56:10 -0400

    update trainer batch file

--- student/sft_trainer.sh
+++ student/sft_trainer.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=c24m170-a100-2 
 #SBATCH --output=./logs/%j_%x.out
 #SBATCH --error=./logs/%j_%x.err
-#SBATCH --time=01:00:00
+#SBATCH --time=04:00:00
 #SBATCH --gres=gpu:a100:2
 #SBATCH --requeue
 
@@ -14,7 +14,7 @@ MODEL_DIR="/scratch/dns5508/model"
 
 # Dataset sizes ("" = full dataset)
 # DATA_SIZES=(128 256 512 1024 "")
-DATA_SIZES=(128 256)
+DATA_SIZES=(512 1024 "")
 # Hyperparameters
 LEARNING_RATES=(1e-4 5e-5)
 BATCH_SIZES=(1 2)
