ba6646e5c3504bb30c7ef263362ec883726d7c7b
Devansh Shah <devanshnshah11@gmail.com>
2026-04-16 02:24:19 -0400

    update SLURM script for learning rate sweep

--- student/grpo_lr_experiments.sh
+++ student/grpo_lr_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=q1-dns5508-lrsweep
-#SBATCH --output=./grpo_logs_dns5508_q1/%j_%x.out
-#SBATCH --error=./grpo_logs_dns5508_q1/%j_%x.err
+#SBATCH --output=./grpo_logs_dns5508_q1/%j_%x_%a.out
+#SBATCH --error=./grpo_logs_dns5508_q1/%j_%x_%a.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=at6646@nyu.edu
 #SBATCH --partition=a100_dev
@@ -10,10 +10,14 @@
 #SBATCH --mem=128G
 #SBATCH --time=4:00:00
 #SBATCH --requeue
+#SBATCH --array=0-3%2
 
 # --- Learning rate sweep ---
-LR_CONFIGS=(1e-5 5e-5 1e-4 5e-4)
+LR_CONFIGS=(1e-5 2e-5 5e-4 1e-4)
 LEARNING_RATE=${LR_CONFIGS[$SLURM_ARRAY_TASK_ID]}
+
+echo "############### Run Log: $(date +%Y-%m-%d_%H:%M:%S) ###############"
+echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
 
 # Configuration
 DATASET_DIR="/gpfs/scratch/an4462/at6646/llmr-a3/data/data-distrib/countdown"
@@ -49,5 +53,7 @@ uv run python /gpfs/scratch/an4462/at6646/dns5508/LLMReasonersAssignment3/studen
   --group_size 8 \
   --gradient_accumulation_steps 8 \
   --epochs_per_rollout_batch 1 \
-  --gpu_memory_utilization 0.85 \
+  --gpu_memory_utilization 0.45 \
   --wandb_run_name "lr_sweep_${LEARNING_RATE}"
+
+echo "Done: $(date +%Y-%m-%d_%H:%M:%S)"
